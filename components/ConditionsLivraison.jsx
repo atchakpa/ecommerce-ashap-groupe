d760737ed702a253b6c579c215250ce787918c9a
ATCHAKPA <atchakpa@gmail.com>
2023-03-05 21:17:20 +0100

    #custum-explorer

--- components/ConditionsLivraison.jsx
+++ components/ConditionsLivraison.jsx
@@ -12,6 +12,7 @@ export default function ConditionsLivraison(){
       width='full'
       justifyContent='center'
       spacing={10}
+      mt={5}
     >
       <HStack
         color='white'
--- components/Expolrer.jsx
+++ components/Expolrer.jsx
@@ -1,14 +1,34 @@
 import data from '@/data'
 import _const from '@/helpers/_const'
-import {Tabs, TabList, Tab, TabPanels, TabPanel, Center, Wrap} from '@chakra-ui/react'
+import {Tabs, TabList, Tab, TabPanels, TabPanel, Center, Wrap, Box, Text} from '@chakra-ui/react'
 import ProductCard from './product/ProductCard'
 
 export default function Explorer() {
   return (
-    <Center mt={5}>
+    <Center 
+      mt={5} 
+      p={24} 
+      flexDirection='column'
+      bg='gray.50'
+      rounded={12}
+      mx={['2%', '15%']}
+      
+    >
+      <Box
+          fontWeight='bold'
+          fontSize='xl'
+          lineHeight='shorter'
+          textAlign='center'
+        >
+          <Text fontSize='4xl'>Explorer</Text>
+          <Text color={`${_const.THEME_COLOR}.500`} fontSize='sm'>
+            Découvrez toutes les catégories
+          </Text>
+        </Box>
       <Tabs
         variant='solid-rounded'
         colorScheme={_const.THEME_COLOR}
+        mt={5}
       >
         <TabList
           bg='gray.100'
